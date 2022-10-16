132713d313460ff0a86e3d70d1efdb5b85a54605
Aaron David <ajdavid128@gmail.com>
2022-10-15 20:13:37 -0500

    commit tree f10c4b1fe91609365eff432d8454293294c9377f

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+
+function saturdayFun(activity){
+    return activity? `This Saturday, I want to ${activity}!`: 'This Saturday, I want to roller-skate!'
+}
+
+function mondayWork(activity){
+    return activity? `This Monday, I will ${activity}.`: `This Monday, I will go to the office.`
+}
